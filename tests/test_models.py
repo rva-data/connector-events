6d38e6f41970081e090508942197729b0fe0ffa6
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2014-08-06 22:56:00 -0400

    Adds manager tests for events
    These will yet need to be updated to acount for timezones.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,10 +1,20 @@
+from datetime import datetime, timedelta
+
 from django.test import TestCase
-from django.core.urlresolvers import reverse
+from django_dynamic_fixture import G
 
 from events.models import Event
 
 
 class ModelTests(TestCase):
+
+    def setUp(self):
+        today = datetime.now()
+        self.past = G(Event, start=today - timedelta(days=2),
+                      end=today - timedelta(days=1))
+        self.inactive = G(Event, is_active=False)
+        self.future = G(Event, start=today + timedelta(days=1),
+                        end=today + timedelta(days=2))
 
     def test_event_uid(self):
         """Ensure the UID is created"""
@@ -12,3 +22,11 @@ class ModelTests(TestCase):
         self.assertFalse(getattr(e, 'uid'))
         e.save()
         self.assertTrue(e.uid)
+
+    def test_active_queryset(self):
+        self.assertEqual(3, Event.objects.all().count())
+        self.assertEqual(2, Event.active.all().count())
+
+    def test_current_events(self):
+        self.assertEqual(3, Event.objects.all().count())
+        self.assertEqual(1, Event.current.all().count())
